89ac9d68e7e9dd31b84628eeade91a67830c6722
Amisha Jha <144998066+amishajha-25@users.noreply.github.com>
2024-01-25 12:51:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,122 @@
+"use strict";
+
+// element toggle function
+const elementToggleFunc = function (elem) {
+  elem.classList.toggle("active");
+};
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () {
+  elementToggleFunc(sidebar);
+});
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  modalContainer.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+// add click event to all modal items
+for (let i = 0; i < testimonialsItem.length; i++) {
+  testimonialsItem[i].addEventListener("click", function () {
+    modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
+    modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
+    modalTitle.innerHTML = this.querySelector(
+      "[data-testimonials-title]"
+    ).innerHTML;
+    modalText.innerHTML = this.querySelector(
+      "[data-testimonials-text]"
+    ).innerHTML;
+    testimonialsModalFunc();
+  });
+}
+
+// add click event to modal close button
+modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+overlay.addEventListener("click", testimonialsModalFunc);
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+  });
+}
+
+
+document.getElementById('downloadBtn').addEventListener('click', function () {
+  // Specify the path to your PDF file in the asset folder
+  var pdfFilePath = 'assets/resume-aman-kumar-sinha.pdf';
+
+  // Fetch the PDF file
+  fetch(pdfFilePath)
+    .then(response => response.blob())
+    .then(blob => {
+      // Create a Blob containing the PDF content
+      var blobUrl = URL.createObjectURL(blob);
+
+      // Create an anchor element and set its attributes
+      var a = document.createElement('a');
+      a.href = blobUrl;
+      a.download = 'resume-aman-kumar-sinha.pdf'; // You can set the file name here
+
+      // Append the anchor element to the document
+      document.body.appendChild(a);
+
+      // Programmatically trigger a click event on the anchor element
+      a.click();
+
+      // Remove the anchor element from the document
+      document.body.removeChild(a);
+
+      // Revoke the Blob URL to free up resources
+      URL.revokeObjectURL(blobUrl);
+    })
+    .catch(error => {
+      console.error('Error fetching the PDF file:', error);
+    });
+});
